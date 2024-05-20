2e032612afb4ca5a17d62d8efae4936422e05c34
YannCardozo <yann_cardozo@hotmail.com>
2024-05-19 23:57:17 -0300

    dando inicio ao desenvolvimento de modal painel usuario

---
+++ JustoFront/wwwroot/Js/modalInterop.js
@@ -0,0 +1,14 @@
+﻿window.modalInterop = {
+    showModal: function (modalId) {
+        const modal = document.getElementById(modalId);
+        if (modal) {
+            modal.style.display = "block";
+        }
+    },
+    closeModal: function (modalId) {
+        const modal = document.getElementById(modalId);
+        if (modal) {
+            modal.style.display = "none";
+        }
+    }
+};
