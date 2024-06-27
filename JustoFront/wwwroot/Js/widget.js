247caaa9e018e40d7f6ef9e5394813fb9432aa80
YannCardozo <yann_cardozo@hotmail.com>
2024-06-27 16:03:52 -0300

    Implementando nova funcionalidade calculadora juridica

---
+++ JustoFront/wwwroot/Js/widget.js
@@ -0,0 +1,11 @@
+﻿function loadWidget() {
+    var script = document.createElement('script');
+    script.src = "https://app.calculojuridico.com.br/script/contribution_time_widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    var div = document.createElement('div');
+    div.id = 'cj-widget';
+    div.setAttribute('data-src', 'https://app.calculojuridico.com.br/embedded/contribution_time_calculation_widgets/start?rel=lp');
+    document.body.appendChild(div);
+}
