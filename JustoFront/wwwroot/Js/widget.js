8b5f65b8f49713144a4cf2a7abdcabc4a6887497
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2024-06-28 15:40:17 -0300

    corrigindo renderizacao do componente em ferramentas

--- JustoFront/wwwroot/Js/widget.js
+++ JustoFront/wwwroot/Js/widget.js
@@ -1,5 +1,9 @@
 ﻿function loadWidget() {
+    // Remove any existing widget if present
+    removeWidget();
+
     var script = document.createElement('script');
+    script.id = 'widget-script';
     script.src = "https://app.calculojuridico.com.br/script/contribution_time_widget.js";
     script.async = true;
     document.body.appendChild(script);
@@ -7,5 +11,17 @@
     var div = document.createElement('div');
     div.id = 'cj-widget';
     div.setAttribute('data-src', 'https://app.calculojuridico.com.br/embedded/contribution_time_calculation_widgets/start?rel=lp');
-    document.body.appendChild(div);
+    document.getElementById('widget-container').appendChild(div);
+}
+
+function removeWidget() {
+    var script = document.getElementById('widget-script');
+    if (script) {
+        document.body.removeChild(script);
+    }
+
+    var div = document.getElementById('cj-widget');
+    if (div) {
+        document.getElementById('widget-container').removeChild(div);
+    }
 }
